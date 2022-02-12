008ff40c3930a99b1ca3de81ef6b7c8f9b940cf6
Pierre Beauguitte <pierre.beauguitte@nb.no>
2022-02-12 12:03:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,178 @@
+const englishText = {
+    'title': 'Astrid & Pierre\'s wedding!',
+    'main-text': `<h2>We are getting married ...</h2>
+<h3>... and we hope you can join us to celebrate!</h3>
+<p>July 9th, at Château de Sarzay (<a href="https://sarzay.net/" target="_blank">website</a>, <a href="https://goo.gl/maps/X4vhXYHizvhCu15D9" target="_blank">map</a>).</p>
+
+	<p>We open the gates at 2pm.</p>
+
+	<p>There will be games, snacks, music and dance, visit of the keep, a ceremony and vin d'honneur, and a dinner!</p>
+<p>Children of all ages are welcome.</p>`,
+    'transport-label': 'Transport',
+    'transport-content': `<p>Sarzay is in the middle of France and literally in the middle of nowhere! Coming by train from Paris, the most accessible city nearby is Châteauroux (35 km) with several departures a day. Pierre’s hometown Issoudun is a little less accessible (but more charming, in our opinion!) by train with only three departures daily. To buy train tickets: <a href="https://www.sncf.com/en" target="_blank">SNCF</a>.</p>
+<p>La Châtre is a village situated 9km from Sarzay and accessible from Châteauroux by bus (bus times <a href="https://www.remi-centrevaldeloire.fr/wp-content/uploads/36-indre/lignes-regulieres/Ligne-F.pdf" target="_blank">here</a>) and there are some B&Bs there.</p>
+
+	    <p>On the wedding day, we will organise transport to the château of Sarzay and back from the following three places: Châteauroux (35 km), Issoudun (45 km) and La Châtre (9 km).</p>
+
+	    <p>Please let us know if you’re interested in this and from which place.</p>`,
+    'accommodation-label': 'Accommodation',
+    'accommodation-content': `<h3>Hotels/B&Bs</h3>
+<p>In Châteauroux and Issoudun there are several hotels and B&Bs in the city centre.</p>
+
+<p>Châteauroux:</p>
+<ul>
+<li><a href="https://all.accor.com/hotel/1080/index.fr.shtml" target="_blank">Ibis Châteauroux</a></li>
+<li><a href="https://www.elyseehotelchateauroux.com/" target="_blank">Elysée Hotel</li>
+<li><a href="https://www.chateauroux-tourisme.com/preparer-mon-sejour/ou-dormir/" target="blank">More options</a> (available in somewhat understandable, auto-translated English)</li>
+</ul>
+
+<p>Issoudun:</p>
+<ul>
+<li><a href="https://www.les3rois.fr/" target="_blank">Les trois rois</a></li>
+<li><a href="https://hotel-de-la-gare-la-locomotive-issoudun.hotelmix.fr/" target="_blank">La locomotive</a></li>
+<li><a href="http://www.la-cognette.com/" target="_blank">La Cognette</a></li>
+<li><a href="https://issoudun.kyriad.com/" target="_blank">Kyriad Issoudun</a></li>
+<li><a href="https://www.tourisme.issoudun.fr/o-dormir" target="_blank">More options (French only)</a>. At the bottom of the page there are two lists of smaller B&Bs and summer houses in the area.</li>
+</ul>
+
+<h3>Camping at the Sarzay castle</h3>
+
+<p>It’s possible to camp at the site, using the chateau’s facilities. Please let us know if you want to do this as we need to let the owners know. We will stay at the site ourselves (but inside!) so we would be delighted if you were staying there!</p>
+
+<h3>Camping at La Châtre</h3>
+
+<p>There's a few cabins at the nearby village La Châtre: <a href="https://www.campingvalvertenberry.com" target="_blank">website</a></p>
+`,
+    'contact': `<p>To contact us: </p>
+<p>Astrid: <a href="mailto:astrid.kristoffersen@gmail.com">astrid.kristoffersen@gmail.com</a>, +47 980 96 570</p>
+<p>Pierre: <a href="mailto:pierre.beauguitte@pm.me">pierre.beauguitte@pm.me</a>, +33 6 80 44 16 36</p>`,
+    'rsvp': 'RSVP - please send by 1. April!',
+    'thingstodo-label': 'Things to do nearby',
+    'thingstodo-content': '<p>Coming soon!</p>'
+};
+
+
+const frenchText = {
+    'title': 'Mariage d\'Astrid & Pierre !',
+    'main-text': `<h2>On se marie ...</h2>
+<h3>... et on espère que vous viendrez célébrer avec nous !</h3>
+<p>Le 9 juillet au Château de Sarzay (<a href="https://sarzay.net/" target="_blank">site</a>, <a href="https://goo.gl/maps/X4vhXYHizvhCu15D9" target="_blank">carte</a>).</p>
+
+	<p>Ouverture des portes à 14h.</p>
+
+	<p>Au programme : jeux, snacks, musique et danse, cérémonie et vin d'honneur, et bien sûr un dîner !</p>
+	<p>Les enfants de tous âges sont bienvenus.</p>`,
+    'transport-label': 'Transport',
+    'transport-content': `<p>Sarzay est en plein cœur de la France, et au milieu de nulle part ! En prenant le train depuis Paris, la ville proche la plus accessible est Châteauroux (35km) avec plusieurs départs par jour. La ville natale de Pierre, Issoudun, est un petit peu moins bien connectée (mais plus charmante, selon nous !) avec seulement 3 départs quotidiens. Pour acheter vos billets de train : <a href="https://www.sncf.com/fr" target="_blank">SNCF</a>.</p>
+<p>La Châtre est un village situé à 9 km de Sarzay et accessible en bus depuis Châteauroux (horaires <a href="https://www.remi-centrevaldeloire.fr/wp-content/uploads/36-indre/lignes-regulieres/Ligne-F.pdf" target="_blank">ici</a>) où il y a quelques gîtes et logements.</p>
+
+<p>Le jour du mariage, nous organiserons un transport aller/retour pour le Château de Sarzay depuis 3 villes : Châteauroux (35km), Issoudun (45km) et La Châtre (9km).</p>
+
+<p>Merci de nous faire savoir si cela vous intéresse, et depuis quel endroit.</p>`,
+    'accommodation-label': 'Hébergement',
+    'accommodation-content': `<h3>Hôtels & gîtes</h3>
+<p>À Châteauroux et Issoudun, il y a plusieurs hôtels et gîtes en centre ville.</p>
+<p>Châteauroux :</p>
+<ul>
+<li><a href="https://all.accor.com/hotel/1080/index.fr.shtml" target="_blank">Ibis Châteauroux</a></li>
+<li><a href="https://www.elyseehotelchateauroux.com/" target="_blank">Elysée Hotel</li>
+<li><a href="https://www.chateauroux-tourisme.com/preparer-mon-sejour/ou-dormir/" target="blank">Autres options</a></li>
+</ul>
+
+<p>Issoudun :</p>
+<ul>
+<li><a href="https://www.les3rois.fr/" target="_blank">Les trois rois</a></li>
+<li><a href="https://hotel-de-la-gare-la-locomotive-issoudun.hotelmix.fr/" target="_blank">La locomotive</a></li>
+<li><a href="http://www.la-cognette.com/" target="_blank">La Cognette</a></li>
+<li><a href="https://issoudun.kyriad.com/" target="_blank">Kyriad Issoudun</a></li>
+<li><a href="https://www.tourisme.issoudun.fr/o-dormir" target="_blank">Plus d'options</a>. Voir en bas de la page pour deux listes de gîtes et chambres d'hôtes dans les environs.</li>
+</ul>
+
+<h3>Camping au château de Sarzay</h3>
+
+<p>Il est possible de poser sa tente sur le site du château. Si c'est ce que vous souhaitez, merci de nous en informer pour qu'on puisse avertir les propriétaires. Nous logerons sur le site du château (mais en intérieur !), donc nous serons ravis si vous voulez rester sur place également.</p>
+
+<h3>Camping à La Châtre</h3>
+
+<p>Il y a quelques châlets disponibles au camping du village voisin de La Châtre : <a href="https://www.campingvalvertenberry.com" target="_blank">site</a></p>
+`,
+    'contact': `<p>Pour nous contacter :</p>
+<p>Astrid : <a href="mailto:astrid.kristoffersen@gmail.com">astrid.kristoffersen@gmail.com</a>, +47 980 96 570</p>
+<p>Pierre : <a href="mailto:pierre.beauguitte@pm.me">pierre.beauguitte@pm.me</a>, +33 6 80 44 16 36</p>`,
+    'rsvp': 'RSVP - à remplir avant le premier avril !',
+    'thingstodo-label': 'À voir ou faire dans les environs',
+    'thingstodo-content': '<p>Coming soon!</p>'
+};
+
+const norwegianText = {
+    'title': 'Astrid & Pierres Bryllup!',
+    'main-text': `<h2>Vi gifter oss ...</h2>
+<h3>... og vi håper du vil feire med oss!</h3>
+<p>9. juli, Château de Sarzay (<a href="https://sarzay.net/" target="_blank">nettside</a>, <a href="https://goo.gl/maps/X4vhXYHizvhCu15D9" target="_blank">kart</a>).</p>
+
+	<p>Dørene åpner klokka 14.</p>
+
+	<p>Det blir leker, snacks, musikk, dans, besøk i borgtårnet, en seremoni, cocktail og middag!</p>
+	<p>Barn i alle aldre er velkomne.</p>`,
+    'transport-label': 'Transport',
+    'transport-content': `<p>Sarzay ligger midt i Frankrike og bokstavelig talt in the middle of nowhere! Hvis du kommer med tog fra Paris, er den mest tilgjengelige byen Châteauroux (35 km) med mange avganger hver dag. Pierres hjemby Issoudun (45 km) er noe mindre tilgjengelig med tog med bare tre avganger daglig (men destod koseligere etter vår mening!). For å kjøpe togbilletter: <a href="https://www.sncf.com/en" target="_blank">SNCF</a>.</p>
+
+<p>La Châtre er en landsby 9 km unna og er tilgjengelig fra Châteauroux med buss (busstider <a href="https://www.remi-centrevaldeloire.fr/wp-content/uploads/36-indre/lignes-regulieres/Ligne-F.pdf" target="_blank">her</a>) og det er noen vertshus der.</p>
+
+<p>På bryllupsdagen organiserer vi transport til og fra slottet fra tre steder: Châteauroux (35 km), Issoudun (45 km) og La Châtre (9 km).</p>
+
+<p>Si fra til oss om du/dere ønsker å benytte dette og fra hvilket sted!</p>`,
+    'accommodation-label': 'Overnatting',
+    'accommodation-content': `<h3>Hoteller & vertshus</h3>
+<p>I Châteauroux og Issoudun er det flere hoteller og vertshus i sentrum.</p>
+<p>Châteauroux :</p>
+<ul>
+<li><a href="https://all.accor.com/hotel/1080/index.fr.shtml" target="_blank">Ibis Châteauroux</a></li>
+<li><a href="https://www.elyseehotelchateauroux.com/" target="_blank">Elysée Hotel</li>
+<li><a href="https://www.chateauroux-tourisme.com/preparer-mon-sejour/ou-dormir/" target="blank">Flere her</a> (Siden er tilgjengelig på direkteoversatt, men relativt forståelig engelsk)</li>
+</ul>
+
+<p>Issoudun :</p>
+<ul>
+<li><a href="https://www.les3rois.fr/" target="_blank">Les trois rois</a></li>
+<li><a href="https://hotel-de-la-gare-la-locomotive-issoudun.hotelmix.fr/" target="_blank">La locomotive</a></li>
+<li><a href="http://www.la-cognette.com/" target="_blank">La Cognette</a></li>
+<li><a href="https://issoudun.kyriad.com/" target="_blank">Kyriad Issoudun</a></li>
+<li><a href="https://www.tourisme.issoudun.fr/o-dormir" target="_blank">Flere her (bare tilgjengelig på fransk)</a>. Nederst på siden er det to lister med vertshus og sommerhus/hytter i området.</li>
+</ul>
+
+<h3>Camping på château de Sarzay</h3>
+
+<p>Det er mulig å campe på området og å bruke slottets fasiliteter. Si fra hvis du ønsker å gjøre det så vi kan si fra til eierne. Vi skal selv overnatte på stedet (men inne!) så vi hadde blitt veldig glad hvis dere også overnattet der!</p>
+
+<h3>Camping i La Châtre</h3>
+
+<p>Det er noen campinghytter i landsbyen La Châtre: <a href="https://www.campingvalvertenberry.com" target="_blank">nettside</a></p>`,
+    'contact': `<p>Kontakt:</p>
+<p>Astrid: <a href="mailto:astrid.kristoffersen@gmail.com">astrid.kristoffersen@gmail.com</a>, +47 980 96 570</p>
+<p>Pierre: <a href="mailto:pierre.beauguitte@pm.me">pierre.beauguitte@pm.me</a>, +33 6 80 44 16 36</p>`,
+    'rsvp': 'RSVP - vennligst send før 1. april!'
+};
+
+function setValues(dict) {
+    document.title = dict['title'];
+    document.getElementById('main-text').innerHTML = dict['main-text'];
+    document.getElementById('transport-label').innerHTML = dict['transport-label'];
+    document.getElementById('transport-content').innerHTML = dict['transport-content'];
+    document.getElementById('accommodation-label').innerHTML = dict['accommodation-label'];
+    document.getElementById('accommodation-content').innerHTML = dict['accommodation-content'];
+    document.getElementById('contact').innerHTML = dict['contact'];
+    document.getElementById('rsvp-text').innerHTML = dict['rsvp'];
+}
+
+function setText() {
+    querystring = window.location.search;
+    if (querystring === '?lang=fr')
+	setValues(frenchText);
+    else if (querystring === '?lang=no')
+	setValues(norwegianText);
+    else
+	setValues(englishText); 
+}
+
+window.onload = setText();
